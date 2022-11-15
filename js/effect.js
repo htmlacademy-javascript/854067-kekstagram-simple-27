c687db16b2b6180077a033c896f10164d25f20f9
inki-na-meeda <the_vlasov@mail.ru>
2022-11-15 22:13:37 +0300

    Помощь друга

---
+++ js/effect.js
@@ -0,0 +1,115 @@
+const form = document.querySelector('.img-upload__form');
+const image = form.querySelector('.img-upload__preview img');
+const sliderElement = form.querySelector('.effect-level__slider');
+const levelEffect = form.querySelector('.effect-level__value');
+
+const EFFECTS = [
+  {
+    name: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+  },
+  {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  {
+    name: 'phobos',
+    style: 'blur',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  {
+    name: 'heat',
+    style: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  }
+];
+
+const DEFAULT_EFFECT = EFFECTS[0];
+let currentEffect = DEFAULT_EFFECT;
+
+const isDefault = () => currentEffect === DEFAULT_EFFECT;
+
+const updateSlider = () => {
+  sliderElement.classList.remove('hidden');
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: currentEffect.min,
+      max: currentEffect.max,
+    },
+    start: currentEffect.max,
+    step: currentEffect.step,
+  });
+
+  if (isDefault()) {
+    sliderElement.classList.add('hidden');
+  }
+};
+
+const onSliderUpdate = () => {
+  image.style.filter = 'none';
+  levelEffect.value = '';
+  image.className = '';
+  if (isDefault()) {
+    return;
+  }
+  const sliderValue = sliderElement.noUiSlider.get();
+  image.style.filter = `${currentEffect.style}(${sliderValue}${currentEffect.unit})`;
+  image.classList.add(`effects__preview--${currentEffect.name}`);
+  levelEffect.value = sliderValue;
+};
+
+const onFormChange = (evt) => {
+  if (evt.target.classList.contains('effects__radio')) {
+    currentEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
+
+    updateSlider();
+  }
+};
+
+const resetEffects = () => {
+  currentEffect = DEFAULT_EFFECT;
+  updateSlider();
+};
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: DEFAULT_EFFECT.min,
+    max: DEFAULT_EFFECT.max,
+  },
+  start: DEFAULT_EFFECT.max,
+  step: DEFAULT_EFFECT.step,
+  connect: 'lower',
+});
+
+form.addEventListener('change', (evt) => onFormChange(evt));
+sliderElement.noUiSlider.on('update', (evt) => onSliderUpdate(evt));
+
+export {resetEffects};
---
+++ js/scale.js
@@ -0,0 +1,33 @@
+const scaleSmaller = document.querySelector('.scale__control--smaller');
+const scaleBigger = document.querySelector('.scale__control--bigger');
+const scaleValue = document.querySelector('.scale__control--value');
+const image = document.querySelector('.img-upload__preview');
+
+const STEP_SCALE = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const DEFAULT_SCALE = 100;
+
+const scaleImage = (value = DEFAULT_SCALE) => {
+  image.style.transform = `scale(${value / 100})`;
+  scaleValue.value = `${value}%`;
+};
+
+const onScaleReduce = () => {
+  const currentValue = parseInt(scaleValue.value, 10);
+  const newValue = (currentValue > MIN_SCALE) ? currentValue - STEP_SCALE : currentValue;
+  scaleImage(newValue);
+};
+
+const onScaleIncrease = () => {
+  const currentValue = parseInt(scaleValue.value, 10);
+  const newValue = (currentValue < MAX_SCALE) ? currentValue + STEP_SCALE : currentValue;
+  scaleImage(newValue);
+};
+
+const resetScale = () => scaleImage();
+
+scaleSmaller.addEventListener('click', () => onScaleReduce());
+scaleBigger.addEventListener('click', () => onScaleIncrease());
+
+export {resetScale};
